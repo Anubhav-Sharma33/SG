4fc358244d304ee8e7c1d1a6e08dba5ef7fec480
Anubhav Sharma <anubhvsharma33@gmail.com>
2025-03-24 14:24:52 +0530

    fifth

--- src/Components/homeSectionThreeDiv.tsx
+++ src/Components/homeSectionThreeDiv.tsx
@@ -5,7 +5,7 @@ export default function HomeSectionThreeDiv(props: any) {
         <div className="absolute bottom-4 right-4 w-8 h-8 bg-white rounded-full flex items-center justify-center text-sm font-bold leading-none hover:bg-green-500 hover:text-white hover:cursor-pointer">
           +
         </div>
-        <img src={props.image} className="w-full h-auto object-cover" />
+        <img src={props.image} className="w-full h-[150px] object-cover" />
       </a>
     </div>
   );
