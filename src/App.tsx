f8433faad3a2c0792dbaf6eb5b2329e470740901
Anubhav Sharma <anubhvsharma33@gmail.com>
2025-03-25 18:26:12 +0530

    error

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import SectionOne from "./UI/HomeSections/sectionOne"
 import SectionThree from "./UI/HomeSections/sectionThree"
 import Footer from "./UI/GeneralSections/footer"
 import Header from "./UI/GeneralSections/header"
-import Togglemenu from "./Components/togglemenu"
 
 function App(){
   return (
