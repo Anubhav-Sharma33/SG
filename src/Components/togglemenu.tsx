d9faeab5f766d27b11d8fb37f2d65afd3279b52d
Anubhav Sharma <anubhvsharma33@gmail.com>
2025-03-26 00:47:30 +0530

    update

--- src/Components/togglemenu.tsx
+++ src/Components/togglemenu.tsx
@@ -10,30 +10,33 @@ export default function Togglemenu() {
   };
 
   return (
-    <ul className="space-y-1 absolute top-full right-0 z-1000 bg-white w-[290px] max-h-[fit] overflow-y-scroll"
-      onClick={(e) => {
-        e.stopPropagation();
-      }}>
-      <li>
-        <a href="#" className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-700">
-          Home
-        </a>
-      </li>
-      <li>
-        <a href="#" className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-700">
-          About Us
-        </a>
-      </li>
+    <div
+      className="absolute top-full right-0 z-1000 bg-white w-[290px] max-h-[400px] overflow-y-auto shadow-lg border border-gray-200 rounded-lg"
+      onClick={(e) => e.stopPropagation()}
+    >
+      <ul className="space-y-1 p-2">
+        <li>
+          <a href="#" className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-700">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-700">
+            About Us
+          </a>
+        </li>
 
-      <MenuWithSubMenu list={list1} index={0} openIndex={openIndex} handleToggle={handleToggle} />
-      <MenuWithSubMenu list={list2} index={1} openIndex={openIndex} handleToggle={handleToggle} />
-      <MenuWithSubMenu list={list3} index={2} openIndex={openIndex} handleToggle={handleToggle} />
-      <MenuWithSubMenu list={list4} index={3} openIndex={openIndex} handleToggle={handleToggle} />
-      <li>
-        <a href="#" className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-          Careers
-        </a>
-      </li>
-    </ul>
+        <MenuWithSubMenu list={list1} index={0} openIndex={openIndex} handleToggle={handleToggle} />
+        <MenuWithSubMenu list={list2} index={1} openIndex={openIndex} handleToggle={handleToggle} />
+        <MenuWithSubMenu list={list3} index={2} openIndex={openIndex} handleToggle={handleToggle} />
+        <MenuWithSubMenu list={list4} index={3} openIndex={openIndex} handleToggle={handleToggle} />
+
+        <li>
+          <a href="#" className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+            Careers
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
